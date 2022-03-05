d5f5cc1f60da4237b512be8520598b646a4794fd
roblesbraun <roblesbraun@gmail.com>
2022-03-05 13:33:49 -0600

    #17-updated-navbar-php

--- pages/sArtistas.php
+++ pages/sArtistas.php
@@ -41,17 +41,11 @@
                             </svg>
                             <span>Home</span>
                         </a>
-                        <a href="./tabs.html" class="flex items-center py-2 px-3 text-black rounded hover:bg-gray-300 hover:text-black transition duration-500">
+                        <a href="./sArtistas.php" class="flex items-center py-2 px-3 text-black rounded hover:bg-gray-300 hover:text-black transition duration-500">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
                             </svg>
                             <span>Tabs</span>
-                        </a>
-                        <a href="./videos.html" class="flex items-center py-2 px-3 text-black rounded hover:bg-gray-300 hover:text-black transition duration-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
-                            </svg>
-                            <span>Videos</span>
                         </a>
                         <a href="./horario.html" class="flex items-center py-2 px-3 text-black rounded hover:bg-gray-300 hover:text-black transition duration-500">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -99,17 +93,11 @@
                     </svg>
                     <span>Home</span>
                 </a>
-                <a href="./tabs.html" class="flex justify-center items-center py-2 px-3 text-black rounded hover:bg-gray-300 hover:text-black transition duration-500">
+                <a href="./sArtistas.php" class="flex justify-center items-center py-2 px-3 text-black rounded hover:bg-gray-300 hover:text-black transition duration-500">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
                     </svg>
                     <span>Tabs</span>
-                </a>
-                <a href="./videos.html" class="flex justify-center items-center py-2 px-3 text-black rounded hover:bg-gray-300 hover:text-black transition duration-500">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
-                    </svg>
-                    <span>Videos</span>
                 </a>
                 <a href="./horario.html" class="flex justify-center items-center py-2 px-3 text-black rounded hover:bg-gray-300 hover:text-black transition duration-500">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
